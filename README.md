74757c796f5120f19809b7dd8a6a660b5a8dfe98
Thomas Heniart <theniart@servicesnomades.com>
2020-05-24 13:19:19 +0200

    First version of prediction model

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Preliloan
 
 Identify the potential customers who have a higher probability of purchasing a personal loan.
+
+## About data in CSV
+
+- ID : unique identifier
+- Personal Loan : did the customer accept the personal load offered (1=Yes, 0=No)
+- Age : customer’s age
+- Experience : number of years of profession experience
+- Income : annual income of the customer ($000)
+- Zip code: home address zip code
+- Family : family size of customer
+- CCAvg : average spending on credit cards per month ($000)
+- Education: education level (1) undergraduate, (2) graduate, (3) advanced/professional
+- Mortgage : value of house mortgage ($000)
+- Securities : does the customer have a securities account with the bank? (1=Yes, 0=No)
+- CDAccount : does the customer have a certificate of deposit with the bank? (1=Yes, 0=No)
+- Online : does the customer use Internet banking facilities (1=Yes, 0=No)
+- CreditCard : does the customer use a credit card issued by Universal Bank? (1=Yes, 0=No)
--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
+import numpy as np
+import pandas as pd
+from pycaret import classification
 
 app = Flask(__name__)
 
+model = classification.load_model(model_name='decision_tree_1')
+columns = ["ID", "Age", "Experience", "Income", "ZIP Code", "Family", "CCAvg", "Education", "Mortgage", "Securities Account",
+           "CD Account", "Online", "CreditCard"]
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template("home.html")
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    form_data = [x for x in request.form.values()]
+    form_data = np.array(form_data)
+    data = pd.DataFrame([form_data], columns=columns)
+    prediction = classification.predict_model(model, data=data)
+    prediction = "Accepted" if bool(prediction.Label[0]) else "Refused"
+    return render_template('home.html', prediction=prediction)
 
 
 if __name__ == '__main__':
