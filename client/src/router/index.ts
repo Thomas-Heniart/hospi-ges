94acc7fb1716dc12ed777978282eeaf588adb312
Thomas Heniart <theniart@servicesnomades.com>
2020-05-29 09:10:04 +0200

    Fixed docker configuration

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import VueRouter, {RouteConfig} from 'vue-router';
+import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '@/views/Home.vue';
 import NotFound from '@/views/NotFound.vue';
 
@@ -18,11 +18,8 @@ const routes: Array<RouteConfig> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  }
+  },
 ];
 
 const router = new VueRouter({
