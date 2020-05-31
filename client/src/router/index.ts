5808abf8c1016d271028ff7b0d89c517ebfe7049
Thomas Heniart <theniart@servicesnomades.com>
2020-05-31 13:27:51 +0200

    Form design integration

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,14 +1,13 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '@/views/Home.vue';
-import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: '*',
-    component: NotFound,
+    redirect: '/',
   },
   {
     path: '/',
